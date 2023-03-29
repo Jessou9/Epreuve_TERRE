433110d1d2eb65a7527351cb52db06d52e178a31
Jessou9 <96484519+Jessou9@users.noreply.github.com>
2023-03-29 12:05:13 +0200

    Create alphabet_a_partir_de.py

---
+++ alphabet_a_partir_de.py
@@ -0,0 +1,5 @@
+#L'alphabet à partir de..
+#Créez un programme qui affiche l'alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d'un retour à la ligne.
+
+for lettre in range(110, 122):
+    print(chr(lettre))
