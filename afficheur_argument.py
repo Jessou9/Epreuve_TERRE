84c429b473c1e093416da8925b976c43e9247702
Jessou9 <96484519+Jessou9@users.noreply.github.com>
2023-03-29 11:45:08 +0200

    Create afficheur_argument.py

---
+++ afficheur_argument.py
@@ -0,0 +1,4 @@
+# Exercice Afficheur d'arguments
+# Créez un programme qui affiche les arguments qu'il reçoit ligne par ligne, peu importe le nombre d'arguments.
+
+print ("Salut") ; print("moi") ; print ("c'est") ; print("Jess")
